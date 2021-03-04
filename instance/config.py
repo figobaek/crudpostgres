d98c8191b893357adb46c2122ffb502c702edb00
figobaek <goldenboyy0524@gmail.com>
2021-03-05 05:11:46 +0900

    gg

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
 # instance/config.py
 
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dt_admin:dt2021@localhost/dreamteam_db2'
+SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://gzwzdcrriuvjmn:0cbbe20a7c6e0dd2a89abaa785c7e51b208e6ac044af5a442517ed06c3c75dc5@ec2-52-71-231-37.compute-1.amazonaws.com:5432/ddrkob1oecjo7m'
 #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://figobaek:1234@localhost/figobaek'
